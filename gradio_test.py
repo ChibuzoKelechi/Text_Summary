e4b5f4d14bc30e33a4f7758bf04120577c696fab
Chibuzo Kelechi <narutokelechi@gmail.com>
2024-02-11 18:42:30 +0100

    gradio interface

--- gradio_test.py
+++ gradio_test.py
@@ -1,4 +1,20 @@
 import gradio
-
 from gradio_client import Client
+from transformers import pipeline
 
+summarizer = pipeline(task="summarization")
+
+
+def summarize_text(text):
+    # summary = summary(text)
+    return 'summary'
+
+
+app = gradio.Interface(
+    title='Text Summarizer',
+    fn=summarize_text,
+    inputs='text',
+    outputs='text'
+)
+
+app.launch()
